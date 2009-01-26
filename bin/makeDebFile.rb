e43bcf1baaa8cba8133b942d684de929dd6fe61c
fbergeron <fbergeron@dc7ab6a9-d314-0410-ba02-f6ca91915298>
2009-01-26 15:17:04 +0000

    Added icon and Installed-Size attribute for deb file.

--- bin/makeDebFile.rb
+++ bin/makeDebFile.rb
@@ -13,8 +13,29 @@ end
 FileUtils.rm_rf( 'debian' )
 FileUtils.mkdir_p( 'debian/DEBIAN' )
 FileUtils.mkdir_p( 'debian/usr/bin' )
-FileUtils.cp( 'etc/deb/control', 'debian/DEBIAN/control' )
-FileUtils.cp( 'toMOTko', 'debian/usr/bin/toMOTko' )
+FileUtils.mkdir_p( 'debian/usr/share/applications' )
+FileUtils.mkdir_p( 'debian/usr/share/pixmaps' )
+FileUtils.cp( 'toMOTko', 'debian/usr/bin' )
+FileUtils.cp( 'etc/deb/toMOTko.desktop', 'debian/usr/share/applications' )
+FileUtils.cp( 'pics/toMOTko.png', 'debian/usr/share/pixmaps' )
+
+# Copy control file and update dynamic data on the fly.
+$SIZE_IN_BYTES =
+    File.size( 'toMOTko' ) + 
+    File.size( 'pics/toMOTko.png' ) 
+$SIZE_IN_KB = $SIZE_IN_BYTES / 1024
+
+File.open( 'etc/deb/control' ) {
+    | input |
+    File.open( 'debian/DEBIAN/control', 'w' ) {
+        | output |
+        input.each_line {
+            | line |
+            modifLine = line.sub( /[$]SIZE_IN_KB/, $SIZE_IN_KB.to_s )
+            output.puts( modifLine )
+        }
+    }
+}
 
 # Build the deb file.
 system( 'dpkg-deb --build debian' )
