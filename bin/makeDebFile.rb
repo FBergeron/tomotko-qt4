72456ee84515d6e2eb12900e018d81aa06e00992
fbergeron <fbergeron@dc7ab6a9-d314-0410-ba02-f6ca91915298>
2009-01-27 09:28:02 +0000

    Improved makeDebFile.rb and added makeRpmFile.rb script.

--- bin/makeDebFile.rb
+++ bin/makeDebFile.rb
@@ -2,12 +2,16 @@
 
 require 'fileutils'
 
-$VERSION = '0.11.2'
+$VERSION  = '0.11.2'
+$DVERSION = '2'
 
 unless( File.exists?( 'toMOTko' ) )
-    $STDERR.put( "The executable file doesn't exist.  Cannot make deb file." )
+    $stderr.puts( "The executable file doesn't exist. Cannot make deb file." )
     exit( -1 )
 end
+
+# prepare (strip) the binary
+system(' strip toMOTko ')
 
 # Create file hierarchy.
 FileUtils.rm_rf( 'debian' )
@@ -15,14 +19,35 @@ FileUtils.mkdir_p( 'debian/DEBIAN' )
 FileUtils.mkdir_p( 'debian/usr/bin' )
 FileUtils.mkdir_p( 'debian/usr/share/applications' )
 FileUtils.mkdir_p( 'debian/usr/share/pixmaps' )
+FileUtils.mkdir_p( 'debian/usr/share/doc' )
+FileUtils.mkdir_p( 'debian/usr/share/doc/tomotko' )
+FileUtils.mkdir_p( 'debian/usr/share/man' )
+FileUtils.mkdir_p( 'debian/usr/share/man/man1' )
 FileUtils.cp( 'toMOTko', 'debian/usr/bin' )
 FileUtils.cp( 'etc/deb/toMOTko.desktop', 'debian/usr/share/applications' )
 FileUtils.cp( 'pics/toMOTko.png', 'debian/usr/share/pixmaps' )
+FileUtils.cp( 'etc/deb/copyright', 'debian/usr/share/doc/tomotko' )
+FileUtils.cp( 'etc/deb/changelog', 'debian/usr/share/doc/tomotko' )
+FileUtils.cp( 'etc/deb/changelog.Debian', 'debian/usr/share/doc/tomotko' )
+FileUtils.cp( 'etc/deb/toMOTko.1', 'debian/usr/share/man/man1' )
+
+system( 'chmod 644 debian/usr/share/applications/toMOTko.desktop' );
+
+# changelog files must be compressed
+system( 'gzip --best debian/usr/share/doc/tomotko/changelog')
+system( 'gzip --best debian/usr/share/doc/tomotko/changelog.Debian')
+
+# compress manual page
+system( 'gzip --best debian/usr/share/man/man1/toMOTko.1')
 
 # Copy control file and update dynamic data on the fly.
 $SIZE_IN_BYTES =
     File.size( 'toMOTko' ) + 
-    File.size( 'pics/toMOTko.png' ) 
+    File.size( 'pics/toMOTko.png' ) +
+    File.size( 'etc/deb/copyright' ) +
+    File.size( 'debian/usr/share/doc/tomotko/changelog.Debian.gz' ) +
+    File.size( 'debian/usr/share/doc/tomotko/changelog.gz' ) +
+    File.size( 'debian/usr/share/man/man1/toMOTko.1.gz' )
 $SIZE_IN_KB = $SIZE_IN_BYTES / 1024
 
 File.open( 'etc/deb/control' ) {
@@ -32,13 +57,17 @@ File.open( 'etc/deb/control' ) {
         input.each_line {
             | line |
             modifLine = line.sub( /[$]SIZE_IN_KB/, $SIZE_IN_KB.to_s )
+            modifLine = line.sub( /[$]VERSION/, "#{$VERSION}-#{$DVERSION}" )
             output.puts( modifLine )
         }
     }
 }
 
 # Build the deb file.
-system( 'dpkg-deb --build debian' )
-FileUtils.mv( 'debian.deb', "toMOTko-#{$VERSION}_i386.deb" )
+system( 'fakeroot dpkg-deb --build debian' )
+FileUtils.mv( 'debian.deb', "tomotko-#{$VERSION}-#{$DVERSION}_i386.deb" )
+
+# Execute the lintian check
+system( 'lintian ' + "tomotko-#{$VERSION}-#{$DVERSION}_i386.deb" )
 
 exit( 0 )
---
+++ bin/makeRpmFile.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+
+require 'fileutils'
+
+$VERSION  = '0.11.2'
+$DVERSION = '2'
+
+unless( File.exists?( "tomotko-#{$VERSION}-#{$DVERSION}_i386.deb" ) )
+    $stderr.puts( "The .deb file doesn't exist. Cannot make .rpm file." )
+    exit( -1 )
+end
+
+# convert the Debian package
+system( "fakeroot alien --to-rpm tomotko-#{$VERSION}-#{$DVERSION}_i386.deb" )
+
+exit( 0 )
